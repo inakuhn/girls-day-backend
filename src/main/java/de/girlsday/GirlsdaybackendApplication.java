963e4cfcdf9a131bb70e50682907105554b6e3a4
rabertol <raina.bertolini@gmail.com>
2016-03-29 13:54:53 +0200

    initial commit

---
+++ src/main/java/de/girlsday/GirlsdaybackendApplication.java
@@ -0,0 +1,12 @@
+package de.girlsday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlsdaybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlsdaybackendApplication.class, args);
+	}
+}
