dad8c643dc1321409ee78c4e78fe1294d3301a27
rabertol <raina.bertolini@gmail.com>
2016-03-29 16:46:10 +0200

    swagger for server application call with http://localhost:8080/swagger-ui.html

--- src/main/java/de/girlsday/api/GreetingController.java
+++ src/main/java/de/girlsday/api/GreetingController.java
@@ -4,13 +4,17 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.girlsday.dal.UserRepository;
 import de.girlsday.model.User;
-
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 @RestController
 public class GreetingController {
 
@@ -19,8 +23,17 @@ public class GreetingController {
 	@Autowired
 	UserRepository userRepository;
 
-	@RequestMapping("/greeting")
-	@ResponseBody
+	 @ApiOperation(value = "getGreeting", nickname = "getGreeting")
+	    @RequestMapping(method = RequestMethod.GET, path="/greeting", produces = "application/json")
+	    @ApiImplicitParams({
+	        @ApiImplicitParam(name = "name", value = "User's name", required = false, dataType = "string", paramType = "query", defaultValue="Niklas")
+	      })
+	    @ApiResponses(value = { 
+	            @ApiResponse(code = 200, message = "Success", response = User.class),
+	            @ApiResponse(code = 401, message = "Unauthorized"),
+	            @ApiResponse(code = 403, message = "Forbidden"),
+	            @ApiResponse(code = 404, message = "Not Found"),
+	            @ApiResponse(code = 500, message = "Failure")}) 
 	public User greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		User user = new User();
 		user.setName(name);
