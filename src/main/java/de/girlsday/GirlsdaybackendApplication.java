ee56f62513b8ed432259b33f68dd7e4a5377a421
rabertol <raina.bertolini@gmail.com>
2016-03-29 18:12:52 +0200

    First implementation of girlsDay

--- src/main/java/de/girlsday/GirlsdaybackendApplication.java
+++ src/main/java/de/girlsday/GirlsdaybackendApplication.java
@@ -12,6 +12,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableSwagger2
 @ComponentScan("de.girlsday")
@@ -20,25 +21,17 @@ public class GirlsdaybackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GirlsdaybackendApplication.class, args);
 	}
-	  @Bean
-	    public Docket newsApi() {
-	        return new Docket(DocumentationType.SWAGGER_2)
-	                .groupName("greetings")
-	                .apiInfo(apiInfo())
-	                .select()
-	                .paths(regex("/greeting.*"))
-	                .build();
-	    }
-	     
-	    private ApiInfo apiInfo() {
-	        return new ApiInfoBuilder()
-	                .title("Spring REST Sample with Swagger")
-	                .description("Spring REST Sample with Swagger")
-	                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
-	                .contact("Niklas Heidloff")
-	                .license("Apache License Version 2.0")
-	                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
-	                .version("2.0")
-	                .build();
-	    }
+
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("new user").apiInfo(apiInfo()).select()
+				.paths(regex("/newuser.*")).build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Spring REST Sample with Swagger")
+				.description("Spring REST Sample with Swagger").termsOfServiceUrl("uc-lib").contact("Ina Kuhn")
+				.license("Apache License Version 2.0")
+				.version("2.0").build();
+	}
 }
